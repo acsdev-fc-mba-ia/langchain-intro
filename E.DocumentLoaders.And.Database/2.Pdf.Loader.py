bf64927ce33ccd2cc2e042cb27ff85a9b4c97752
Allan Santos <allancarneirosantos@gmail.com>
2025-08-31 02:42:28 +0000

    feat: folder E added

---
+++ E.DocumentLoaders.And.Database/2.Pdf.Loader.py
@@ -0,0 +1,14 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+loader = PyPDFLoader("./gpt5.pdf")
+docs = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+
+chunks = splitter.split_documents(docs)
+
+# for chunk in chunks:
+#     print(chunk)
+#     print("-"*30)
+print(len(chunks))
